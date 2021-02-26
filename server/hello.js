b500a5ea757887dee395c6bf968233503b2baf3e
KrishnaVyshak <79155778+KrishnaVyshak@users.noreply.github.com>
2021-02-26 12:56:50 +0530

    Create hello.js

---
+++ server/hello.js
@@ -0,0 +1,13 @@
+exports.handler = async (event, context) => {
+
+  const name = event.queryStringParameters.name || "World";
+
+  return {
+
+    statusCode: 200,
+
+    body: `Hello, ${name}`,
+
+  };
+
+};
