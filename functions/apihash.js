bf5d3cddec3b2aaa2d41ee173bc0e2d773e5832c
KrishnaVyshak <79155778+KrishnaVyshak@users.noreply.github.com>
2021-02-26 15:35:24 +0530

    Update apihash.js

--- functions/apihash.js
+++ functions/apihash.js
@@ -1,16 +1,30 @@
-exports.handler = async function (event, context, callback) {
-  if (event.httpMethod !== 'GET') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
+exports.handler = async function(event) {
+
+  // We only care to do anything if this is our POST request.
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode,
+      headers,
+      body: "This was not a POST request!"
+    };
   }
 
-  try {
-    const data = await getData();
-    const stringified = JSON.stringify(data);
-    const returnObj = { statusCode: 200, body: stringified };
-    console.log('returnObj:', JSON.stringify(returnObj, null, 2));
-    return returnObj;
-  } catch (error) {
-    console.error(error);
-    return { statusCode: 500, body: `An error occurred: ${String(error)}` };
+  // Parse the body contents into an object.
+  const data = JSON.parse(event.body);
+
+  // Make sure we have all required data. Otherwise, get outta here.
+  if (!data.token || !data.amount || !data.idempotency_key) {
+    const message = "Required information is missing!";
+
+    console.error(message);
+
+    return {
+      statusCode,
+      headers,
+      body: JSON.stringify({
+        status: "failed",
+        message
+      })
+    };
   }
 };
