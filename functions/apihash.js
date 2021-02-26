448609ce0f6aa3a83b0756d90aab2ec7332e5b66
KrishnaVyshak <79155778+KrishnaVyshak@users.noreply.github.com>
2021-02-26 15:44:14 +0530

    Update apihash.js

--- functions/apihash.js
+++ functions/apihash.js
@@ -2,6 +2,6 @@ exports.handler = async (event, context) => {
   const { api } = event.queryStringParameters
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: `Hello ${api || 'World'}` })
+    body: JSON.stringify({ API_KEY: ` ${api || 'NO VALID INPUT FOUND', "STATUS" : "200"}` })
   }
 }
