592f2b5fe71032c6008b3a08de3eac46640723f5
KrishnaVyshak <79155778+KrishnaVyshak@users.noreply.github.com>
2021-02-27 14:28:01 +0530

    Update apihash.js

--- functions/apihash.js
+++ functions/apihash.js
@@ -2,6 +2,6 @@ exports.handler = async (event, context) => {
   const { api } = event.queryStringParameters
   return {
     statusCode: 200,
-    body: JSON.stringify({ API_KEY: ` ${api || 'NO VALID INPUT FOUND'}`, { "TYPE" : "user", "HOST" : "sketchon.ml", "STATUS" : "200"})
+    body: JSON.stringify({ API_KEY: ` ${api || 'NO VALID INPUT FOUND'}`, "TYPE" : "user", "HOST" : "sketchon.ml", "STATUS" : "200"})
   }
 }
