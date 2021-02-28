6627fc59ad57bc7627acafd48518152aa2eea5b1
KrishnaVyshak <79155778+KrishnaVyshak@users.noreply.github.com>
2021-02-28 12:17:54 +0530

    Update index.js

--- suspense/create/index.js
+++ suspense/create/index.js
@@ -44,8 +44,8 @@ const generatePDF = async (name) => {
 
   // Draw a string of text diagonally across the first page
   firstPage.drawText(name, {
-    x: 260,
-    y: 100,
+    x: 360,
+    y: 10,
     size: 58,
     font: SanChezFont,
     color: rgb(0.2, 0.84, 0.67),
@@ -62,7 +62,7 @@ const generatePDF = async (name) => {
 
   var file = new File(
     [pdfBytes],
-    "Padhega India Subscription Certificate.pdf",
+    "sketchOn Developer Participation Certificate.pdf",
     {
       type: "application/pdf;charset=utf-8",
     }
