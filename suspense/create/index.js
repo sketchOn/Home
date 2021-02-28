f6f43b9abc9de71a89f778e2e4b7fb8403721792
KrishnaVyshak <79155778+KrishnaVyshak@users.noreply.github.com>
2021-02-28 12:29:57 +0530

    Update index.js

--- suspense/create/index.js
+++ suspense/create/index.js
@@ -44,8 +44,8 @@ const generatePDF = async (name) => {
 
   // Draw a string of text diagonally across the first page
   firstPage.drawText(name, {
-    x: 250,
-    y: 108,
+    x: 0,
+    y: 0,
     size: 58,
     font: SanChezFont,
     color: rgb(0.2, 0.84, 0.67),
