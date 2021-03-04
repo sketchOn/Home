24db9597cd3231ded0d9b0cf67a8268d6fe6827a
KrishnaVyshak <79155778+KrishnaVyshak@users.noreply.github.com>
2021-03-04 19:47:04 +0530

    Update apihash.js

--- functions/apihash.js
+++ functions/apihash.js
@@ -1,5 +1,15 @@
+const querystring = require("querystring");
+
 exports.handler = async (event, context) => {
-  const name = event.queryStringParameters.name || "World";
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  // When the method is POST, the name will no longer be in the event’s
+  // queryStringParameters – it’ll be in the event body encoded as a query string
+  const params = querystring.parse(event.body);
+  const name = params.name || "World";
 
   return {
     statusCode: 200,
